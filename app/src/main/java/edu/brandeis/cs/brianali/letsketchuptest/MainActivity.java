82dc9591861589a26d566929d264d84757f07f84
briana-li <brianali@brandeis.edu>
2017-11-28 18:22:13 -0500

    Initial commit

---
+++ app/src/main/java/edu/brandeis/cs/brianali/letsketchuptest/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.brandeis.cs.brianali.letsketchuptest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
